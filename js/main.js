354c72f5d6e702be0d4b59632c71e972e1e287df
efectoTam <tam.luque@gmail.com>
2016-12-14 12:58:22 -0300

    Avance del trabajo

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ function validarCampoTexto(input){
 		mensaje += "Campo Obligatorio"	
 	}else{
 		if($(input).val().length > 30){
-			mensaje += "Debe tener menos de 30 caracteres "
+			mensaje += "Debe tener menos de 30 caracteres";
 		}
 		if($(input).val().charAt(0).toUpperCase() != $(input).val().charAt(0)){
-			mensaje += "Primera letra en mayúscula "
+			mensaje += "Primera letra en mayúscula";
 		}
 		if( ($(input).val().match(/^[a-zA-Z]+$/))){
-			mensaje += "solo debo tener letras"
+			mensaje += "solo debo tener letras";
 		}
 	}
 
@@ -18,19 +18,50 @@ function validarCampoTexto(input){
 	var span_nombre = $("<span>" + mensaje + "</span>");
 	$(input).parent().append(span_nombre);
 }
+
 function validarCampoEmail(input){
 	var mensaje = "";
 	if($(input).val() == ""){
-		mensaje += "Campo Obligatorio"	
+		mensaje += "Campo Obligatorio";
 	}else{
 		if(!($(input).val().match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/))){
-			mensaje += "Formato Inválido"
+			mensaje += "Formato Inválido";
 		}
 	}
 	$(input).siblings().filter("span").remove();
 	var span_nombre = $("<span>" + mensaje + "</span>");
 	$(input).parent().append(span_nombre);
 }
+
+function validarCampoContraseña(input){
+	var mensaje = "";
+	if($(input).val() == ""){
+		mensaje += "Campo Obligatorio";
+	} else{
+		if($(input).val().length < 6){
+			mensaje += "Debe tener más de 6 caracteres";
+		}
+
+		if($(input).val()=="password"||123456||098754){
+			mensaje += "El campo contraseña no puede ser igual a 'password' o '123456' o '098754'";
+		}
+	}
+	$(input).siblings().filter("span").remove();
+	var span_nombre = $("<span>" + mensaje + "</span>");
+	$(input).parent().append(span_nombre);
+}
+
+function validarCampoCheckbox(input){
+	var mensaje = "";
+	if($(input).val() /**/){
+		mensaje += "Debe seleccionar al menos un tipo de bici.";
+	}
+	$(input).siblings().filter("span").remove();
+	var span_nombre = $("<span>" + mensaje + "</span>");
+	$(input).parent().append(span_nombre);
+}
+
+
 
 $( ".form-signup" ).submit(function(e) {
   e.preventDefault();
@@ -39,8 +70,11 @@ $( ".form-signup" ).submit(function(e) {
   	if($(this).attr('type') == 'text'){
   		validarCampoTexto($(this));
   	}else if($(this).attr('type') == 'email'){
-  		console.log("Hola");
   		validarCampoEmail($(this));
+  	}else if($(this).attr('type') == 'password'){
+  		validarCampoContraseña($(this));
+  	}else if($(this).attr('type') =='checkbox'){
+  		validarCampoCheckbox($(this));
   	}
   })
 });
